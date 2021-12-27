0b949a8fd3cdb702707276f2ed9a0d1b7bc7f3d7
SUNG WOOK HWANG <destiny0810@naver.com>
2021-12-27 12:57:23 +0900

    Fix : 폴더 파일 갯수 셀 때에 폴더 제외하기 위한 코드 추가

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,15 @@ import archiver from 'archiver';
 const zipFolder = async () => {
   // 압축할 경로를 지정합니다.
   const homedir = os.homedir();
-  const output = fs.createWriteStream(homedir + '/example.zip');
+  const output = fs.createWriteStream(homedir + '/sampleZip.zip');
 
-  // 파일 갯수 세기 위해 사용 (어차피 DB에서 가져올때 카운트해서 필요 없긴 함.)
-  const folderResult = await glob.promise(`${homedir}/zipTest/hugeFolder/**`);
+  // 파일 갯수 세기 위해 사용 (어차피 DB에서 가져올때 카운트해서 필요 없긴 함. && 폴더 갯수도 카운팅 필요 없음)
+  const folderResult = await glob.promise(
+    `${homedir}/zipTest/sampleFolder/**`,
+    {
+      nodir: true,
+    }
+  );
   const totalFiles = folderResult.length;
 
   // 압축 옵션을 설정합니다.
@@ -23,16 +28,18 @@ const zipFolder = async () => {
 
   archive.pipe(output);
   // 압축할 폴더를 집어 넣어봅시다.
-  archive.directory(`${homedir}/zipTest/hugeFolder`, false).finalize();
+  archive.directory(`${homedir}/zipTest/sampleFolder`, false).finalize();
   // 압축의 경과를 보기 위해서 정의
   output.on('open', () => {
     console.log('시작 되었습니다. 폴더 압축 ', totalFiles);
   });
   archive.on('progress', (data) => {
     const { entries, fs } = data;
-    // console.log('ENTRIES : {}', entries.processed);
-    // console.log('ENTRIES', entries);
-    // console.log('FS', fs);
+    const progressStatus = ((entries.processed / totalFiles) * 100).toFixed(0);
+    const remainFile = totalFiles - entries.processed;
+    console.log(
+      `PERCENT : ${progressStatus} | PROCESSED FILE : ${entries.processed} | REMAIN FILE : ${remainFile}`
+    );
   });
   archive.on('finish', () => {
     console.timeEnd('FOLDER_SAVE');
